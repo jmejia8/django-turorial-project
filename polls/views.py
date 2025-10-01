1cbafbbc77acd88ba612d9f63783b3e737a85af4
Jesus Mejia <jesusmejded@gmail.com>
2025-10-01 11:31:01 -0600

    update views

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
@@ -9,8 +9,8 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    response = "Your are looking at question %s" % question_id
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "Your are looking at results of %s" % question_id
