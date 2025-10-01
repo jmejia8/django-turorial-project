719e383ef58ac0780718cba7ca4e51cc46420884
Jesus Mejia <jesusmejded@gmail.com>
2025-10-01 13:42:48 -0600

    voting system

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
+from django.db.models import F
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -13,9 +15,16 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    response = "Your are looking at results of %s" % question_id
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    response = "Your are voting question %s" % question_id
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {"question": question, "error_message": "First, select a choice"})
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
